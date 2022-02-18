5bfa289d83924cef2f9a8e03b42ed9e049cd0c00
ahDevGit <justalihaider@gmail.com>
2022-02-18 17:04:51 +0600

    getValue() function added

--- js/script.js
+++ js/script.js
@@ -1,24 +1,40 @@
+function getValue(valueName) {
+  const targetInputField = document.getElementById(`${valueName}`);
+  const inputFieldValue = parseFloat(targetInputField.value);
+  return inputFieldValue
+}
+
 document.getElementById('calculate-budget').addEventListener('click', function () {
-
-  const monthlyIncomeField = document.getElementById('monthly-income');
-  const monthlyIncome = parseFloat(monthlyIncomeField.value);
-
-  /* Food Expens Value */
-  const foodExpensValue = document.getElementById('food');
-  const foodExpens = parseFloat(foodExpensValue.value);
-  /* Rent Expens Value */
-  const rentExpensValue = document.getElementById('rent');
-  const rentExpens = parseFloat(rentExpensValue.value);
-  /* Clothes Expens Value */
-  const clothesExpensValue = document.getElementById('clothes');
-  const clothesExpens = parseFloat(clothesExpensValue.value);
+  // Monthly Income Value
+  getValue('monthly-income');
+  // Food Expense Value
+  getValue('food');
+  // Rent Expense Value
+  getValue('rent');
+  // Clothes Expense Value
+  getValue('clothes');
 
   /* Total Expenses */
-  const totalExpenses = foodExpens + rentExpens + clothesExpens;
+  const totalExpenses = getValue('food') + getValue('rent') + getValue('clothes');
   document.getElementById('total-expenses').innerHTML = totalExpenses;
 
   /* Balance After Expenses */
-  const balanceAfterExpenses = monthlyIncome - totalExpenses;
+  const balanceAfterExpenses = getValue('monthly-income') - totalExpenses;
   document.getElementById('balance-after-expenses').innerHTML = balanceAfterExpenses;
 })
 
+// document.getElementById('btn-savings').addEventListener('click', function () {
+//   const monthlyIncomeField = document.getElementById('monthly-income');
+//   const monthlyIncome = parseFloat(monthlyIncomeField.value);
+//   const saveInput = document.getElementById('save-input');
+//   const savingAmount = monthlyIncome / 100 * parseFloat(saveInput.value);
+//   const savingAmoutValue = document.getElementById('savings-amount');
+//   savingAmoutValue.innerHTML = savingAmount;
+
+//   /* Remaining Balance */
+//   const balanceAfterExpenses = monthlyIncome - totalExpenses;
+//   const remainingBalance = document.getElementById('remaining-balance');
+//   const remainingBalanceReport = balanceAfterExpenses - savingAmount;
+//   console.log(remainingBalanceReport);
+//   remainingBalance.innerHTML = remainingBalanceReport;
+// })
